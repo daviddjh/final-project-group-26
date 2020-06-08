fcb7e7f5ba0e228234b16a63b48e2921ea461ad3
David Headrick <david.headrick9@gmail.com>
2020-06-07 21:58:00 -0700

    improved server, added comments, added links to index

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//set server port
  var port_number = 3000;
  if(process.env.PORT){
      port_number = process.env.PORT;
@@ -30,60 +31,100 @@ app.engine('handlebars', exphbs({defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 
+//serve static files
 app.use(express.static(__dirname + "/public"));
 
-//serve static files
 app.get('/', function(req, res, next){
-  res.status(200).render(page, {
-    posts: 'all posts from mongo DB'
-  });
+  res.redirect("/all");
 });
 
 //serve a page with many posts
 app.get('/:page', function(req, res, next){
   var page = req.params.page;
-  var pagesDB = db.collection('pages');
-  var pagesCursor = pagesDB.find({});
 
   // makes sure page is valid
   if(pages.includes(page)){
-    //temp
-    res.status(200).sendFile(__dirname + "/public/index.html");
 
-    /*
-    //send this page with posts
-    res.status(200).render(page, {
-      posts: 'page\'s posts from mongo DB'
-    });
-    */
-  } else {
-    next();
-  }
-});
+    //get pages from DB
+    var pagesDB = db.collection('pages');
+    var pagesCursor = pagesDB.find({});
 
-//serve a specific post's page with comments
-app.get('/:page/:postID', function(req, res, next){
-  console.log("requested post page");
-  var page = req.params.page;
-  var postID = req.params.postID;
-  var post_OID = new ObjectID(postID);
-  var postDB = db.collection('posts');
-  var postCursor = postDB.find({_id: post_OID});
+    //get posts from DB
+    var postDB = db.collection('posts');
+    if(page != "all"){
+      var postCursor = postDB.find({page: {$in: [page]}}).sort({votes: -1});
+    } else {
+      var postCursor = postDB.find({}).sort({votes: -1});
+    }
 
-  //checks if page is real
-  if(pages.includes(page)){
-
+    //turn post into array
     postCursor.toArray(function (err, postDocs){
       console.log(postDocs);
 
       if(err) {
         res.status(500).send("Error fetching pages from DB")
+
       } else {
-          if(postDocs){
+        if(postDocs){
+          //temp page
+          res.status(200).sendFile(__dirname + "/public/index.html");
+
+          /*
+          //render and send the post's page
+          res.status(200).render(postPage, {
+            post: post
+          });
+          */
+        } else {
+          console.log("couldn't find posts in DB")
+          next();
+        }
+      }
+    });
+  } else {
+    console.log("couldn't find page" + page );
+    next();
+  }
+});
+
+//serve a specific post's page with comments
+app.get('/:postID', function(req, res, next){
+  console.log("requested post page");
+
+  //get posts from server
+  var postID = req.params.postID;
+  var post_OID = new ObjectID(postID);
+  var postDB = db.collection('posts');
+  var postCursor = postDB.find({_id: post_OID});
+
+  //get commnet from server
+  var commentDB = db.collection('comments');
+  var commentCursor = commentDB.find({postID: post_OID});
+
+  //turn post into array
+  postCursor.toArray(function (err, postDocs){
+    console.log("post: ");
+    console.log(postDocs);
+
+    if(err) {
+      res.status(500).send("Error fetching post from DB")
+    } else {
+
+
+      //turn comments to array 
+      commentCursor.toArray(function (err, commentDocs){
+        if(err) {
+          res.status(500).send("Error fetching comments from DB")
+        } else {
+
+          if(commentDocs){
             //temp
             res.status(200).sendFile(__dirname + "/public/index.html");
+            console.log("comments: ");
+            console.log(commentDocs);
+
             /*
-            //send the post's page
+            //render post's page and send
             res.status(200).render(postPage, {
               post: post
             });
@@ -92,12 +133,10 @@ app.get('/:page/:postID', function(req, res, next){
             console.log("couldn't find post in DB")
             next();
           }
-      }
-    });
-  } else {
-    console.log("couldn't find page" + page + " in DB")
-    next();
-  }
+        }
+      });
+    }
+  });
 });
 
 //serve 404 page
@@ -139,13 +178,14 @@ app.post('/:page/:postID/addComment', function (req, res){
     var postCursor = postDB.find({_id: post_OID});
     var commentsDB = db.collection('comments');
 
-    //find post
+    //find post to make sure it is real
     postCursor.toArray(function (err, postDocs){
       console.log(postDocs);
 
       if(err) {
         res.status(400).send("Post Not Found");
       } else {
+          //ensure post is real
           if(postDocs){
             //add comment
             commentsDB.insertOne({
@@ -171,19 +211,28 @@ app.post('/:page/:postID/addComment', function (req, res){
   });
 
 
+//establish connection with MongoDB server
 MongoClient.connect(mongoURL, function (err, client) {
   if(err) {
     throw err;
   }
+
+  //if successfull, continue setting up server
   db = mongoDBDatabase = client.db(mongoDBName);
 
   //get pages from db
   var pagesDB = db.collection('pages');
   var pagesCursor = pagesDB.find({});
   pagesCursor.toArray(function (err, pageDocs){
-    for (i = 0; i < Object.keys(pageDocs).length; i++ ){
-      pages.push(pageDocs[i].page);
+    if(err){
+      console.log(err);
+    } else {
+      //add pages to pages array
+      for (i = 0; i < Object.keys(pageDocs).length; i++ ){
+        pages.push(pageDocs[i].page);
+      }
+      //start server
+      app.listen(port, () => console.log("listening on port " + port));
     }
-    app.listen(port, () => console.log("listening on port " + port));
   });
 });
