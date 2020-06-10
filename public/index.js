6b93d20e008607be10d19081aa962ee04a327914
David Headrick <david.headrick9@gmail.com>
2020-06-10 13:21:32 -0700

    client side update and icon

--- public/index.js
+++ public/index.js
@@ -1,75 +1,171 @@
-function insertNewQuestion(questionText, questionAuthor) {
-  var newQuestion = {
-    content: questioncontent,
-    tags: tags
-  };
-  var newQuestion = Handlebars.templates.Question-Container(QuestionContainer);
-  
-  var questionContainer = document.querySelector("Question-Container");
-  questionContainer.insertAdjacentHTML("beforeend",newQuestion);
+Handlebars.registerHelper('convertTag', function(a){
+  if (a === "sports"){
+    return "Sports";
+  } else if (a == "animals"){
+    return "Animals";
+  } else if (a == "news"){
+    return "News";
+  } else if (a == "politics"){
+    return "Politics";
+  } else if (a == "gaming"){
+    return "Gaming";
+  }
+});
+
+var tags = document.getElementsByClassName('tagCheck');
+for (tagCheck of tags){
+  tagCheck.addEventListener('change', function(){
+    if(this.checked){
+
+      for (tag of tags){
+        if (tag != this){
+          tag.checked = false;
+        }
+
+      }
+
+    }
+  })
 }
 
-function insertNewComment(commentText, commentAuthor)
-{
+function insertQuestion(questionTitle, questionText, questionAuthor, tagsChecked) {
 
+  var questionData = {
+    title: questionTitle,
+    text: questionText,
+    userID: questionAuthor,
+    tag: tagsChecked
+  }
+
+  var request = new XMLHttpRequest();
+  request.open('POST', '/addPost');
+  var requestBody = JSON.stringify(questionData);
+  console.log(requestBody);
+  request.setRequestHeader(
+    'Content-Type', 'application/json'
+  );
+
+  request.addEventListener('load', function(event){
+    var message = event.target.response;
+    if(event.target.status != 200){
+      alert(message);
+    } else {
+
+    console.log(message);
+    console.log(typeof message);
+    questionData._id = message.replace(/\"/g, "");
+
+    var questionTemplate = Handlebars.templates.post;
+    var newQuestionHtml = questionTemplate(questionData);
+
+    var noPostsAlert =  document.getElementById('no-posts');
+    if(noPostsAlert){
+        noPostsAlert.classList.add('hidden');
+    }
+
+    var questionContainer = document.querySelector('main.Question-Container');
+    questionContainer.insertAdjacentHTML('afterbegin', newQuestionHtml);
+
+    }
+  });
+
+  request.send(requestBody);
 }
-
 
 function handleModalAcceptClick() {
 
-
-}
-
-/*
- * This function shows the modal to create a question when the "create question"
- * button is clicked.
- */
-function showCreatequestionModal() {
-  var modalBackdrop = document.getElementById('modal-backdrop');
-  var createQuestionModal = document.getElementById('create-question-modal');
-
-  // Show the modal and its backdrop.
-  modalBackdrop.classList.remove('hidden');
-  createQuestionModal.classList.remove('hidden');
-
-}
-
-
-/*
- * This function clears any value present in any of the question input elements.
- */
-function clearquestionInputValues() {
-  var twitInputElems = document.getElementsByClassName('question-input-element');
-  for (var i = 0; i < twitInputElems.length; i++) {
-    var input = twitInputElems[i].querySelector('input, textarea');
-    input.value = '';
+  var questionTitle = document.getElementById('question-title-input').value;
+  var questionText = document.getElementById('question-text-input').value;
+  var questionAuthor = document.getElementById('question-author-input').value;
+  var tagsChecked = [];
+  if(tags){
+    for (i = 0; i < tags.length; i++){
+      if(tags[i].checked){
+        tagsChecked.push(tags[i].value);
+      }
+    }
   }
 
-  var twitInputElems = document.getElementsByClassName('tag-input-element');
-  for (var i = 0; i < twitInputElems.length; i++) {
-    var input = twitInputElems[i].querySelector('input, textarea');
-    input.value = '';
+  if (questionText && questionAuthor && questionTitle && tagsChecked.length > 0) {
+    //api stuff
+
+    console.log(tagsChecked);
+    insertQuestion(questionTitle, questionText, questionAuthor, tagsChecked);
+
+    hideNewPostModal();
+
+  } else {
+
+    alert('You must specify every field!');
+
   }
+}
+
+var qmodalBack = document.getElementById("modal-backdrop");
+var qmodalScreen = document.getElementById("create-question-modal");
+
+function clearNewPostModal(){
+  var questionTitle = document.getElementById('question-title-input');
+  var questionText = document.getElementById('question-text-input');
+  var questionAuthor = document.getElementById('question-author-input');
+
+  questionAuthor.value = '';
+  questionText.value = '';
+  questionTitle.value = '';
+
+  for (tag of tags){
+    tag.checked = false;
+  }
+}
+function unhideNewPostModal(){
+
+  qmodalBack.classList.remove('hidden');
+  qmodalScreen.classList.remove('hidden');
+  /*
+  qmodalBack.style.display = "block";
+  qmodalScreen.style.display = "block";
+  */
+
+}
+
+function hideNewPostModal(){
+
+  qmodalBack.classList.add('hidden');
+  qmodalScreen.classList.add('hidden');
+  /*
+  qmodalBack.style.display = "none";
+  qmodalScreen.style.display = "none";
+  */
+
+  clearNewPostModal();
 
 }
 
 
-/*
- * This function hides the modal to create a question and clears any existing
- * values from the input fields whenever any of the modal close actions are
- * taken.
- */
-function hideCreatequestionModal() {
-  var modalBackdrop = document.getElementById('modal-backdrop');
-  var createQuestionModal = document.getElementById('create-question-modal');
+var createquestion = document.getElementById("create-question-button");
+createquestion.addEventListener('click', function(event) {
+  console.log("== The create question button was clicked");
 
-  // Show the modal and its backdrop.
-  modalBackdrop.classList.add('hidden');
-  createQuestionModal.classList.add'hidden');
-}
-
-function parsequestionElem(questionElem) {
+  unhideNewPostModal();
 
 
+});
 
-}
+var modelReturn = document.getElementsByClassName("modal-return-button")[0];
+modelReturn.addEventListener('click', function(event) {
+  console.log("== The close modal button was clicked");
+
+  hideNewPostModal();
+
+});
+
+var modelClose = document.getElementsByClassName("modal-close-button")[0];
+modelClose.addEventListener('click', function(event) {
+  console.log("== The close modal button was clicked");
+
+  hideNewPostModal();
+
+});
+
+var modelAccept = document.getElementsByClassName("modal-add-button")[0];
+modelAccept.addEventListener('click', handleModalAcceptClick);
